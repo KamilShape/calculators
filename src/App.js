5301cfe142e54a205b30f86b6c6dccd4927f2153
KamilShape <kamilshape@gmail.com>
2022-09-16 12:08:23 +0200

    change to HashRouter

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import {React,Component} from 'react'
-import {BrowserRouter as Router, NavLink, Route, Routes} from 'react-router-dom'
+import { HashRouter as Router, NavLink, Route, Routes} from 'react-router-dom'
 import './App.css';
 import StartButton from './components/StartButton'
 import Start from './layouts/Start'
@@ -27,16 +27,16 @@ class App extends Component {
       <div className="app">
         <Router>
           <div className='app_nav'>
-            <div className='app_link' onClick={()=>this.changeVisibility('buttons')}><NavLink to="calculators/"><StartButton name='Start' picture='fas fa-play-circle'/></NavLink> </div>
-            <div className='app_link' onClick={()=>this.changeVisibility('buttons')}><NavLink to="calculators/math"><StartButton name='Math' picture='fas fa-calculator'/></NavLink> </div>
-            <div className='app_link' onClick={()=>this.changeVisibility('buttons')}><NavLink to="calculators/health"><StartButton name='Physics' picture='fas fa-brain'/></NavLink></div>
-            <div className='app_link' onClick={()=>this.changeVisibility('buttons')}><NavLink to="calculators/sports"><StartButton name='Sport' picture='fas fa-running'/></NavLink></div>
+            <div className='app_link' onClick={()=>this.changeVisibility('buttons')}><NavLink to="/"><StartButton name='Start' picture='fas fa-play-circle'/></NavLink> </div>
+            <div className='app_link' onClick={()=>this.changeVisibility('buttons')}><NavLink to="/math"><StartButton name='Math' picture='fas fa-calculator'/></NavLink> </div>
+            <div className='app_link' onClick={()=>this.changeVisibility('buttons')}><NavLink to="/health"><StartButton name='Physics' picture='fas fa-brain'/></NavLink></div>
+            <div className='app_link' onClick={()=>this.changeVisibility('buttons')}><NavLink to="/sports"><StartButton name='Sport' picture='fas fa-running'/></NavLink></div>
           </div>
           <Routes>
-            <Route path='calculators/'element={<Start/>}/>  
-            <Route path='calculators/math/*'element={<Math/>}/>
-            <Route path='calculators/health/*'element={<Physics/>}/>
-            <Route path='calculators/sports/*' element={<Sports/>}/>
+            <Route path='/'element={<Start/>}/>  
+            <Route path='/math/*'element={<Math/>}/>
+            <Route path='/health/*'element={<Physics/>}/>
+            <Route path='/sports/*' element={<Sports/>}/>
           </Routes>
         </Router>
       </div>
